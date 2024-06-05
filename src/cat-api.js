6615df1f365a86d8cd46b8bc8745397e03a89290
Valerii Arlamovskyi <valera.arl96@gmail.com>
2024-06-04 20:15:58 -0400

    add new fuctions

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,25 @@
+import axios from 'axios';
 
+const URL = 'https://api.thecatapi.com/v1';
+const BREEDS_ENDPOINT = '/breeds';
+const KEY =
+  'live_B9grJgv4HqzW6TZpqeLqnlyqi2vhD7LVDuhx5sgYT8L76L0WiuGpLioEPRoZa1XP';
 
+export function fetchBreeds() {
+  return axios.get(URL + BREEDS_ENDPOINT, {
+    headers: {
+      'x-api-key': KEY,
+    },
+  });
+}
 
-
-
+export function fetchCatByBreed(breedId) {
+  return axios.get(URL + '/images/search?', {
+    headers: {
+      'x-api-key': KEY,
+    },
+    params: {
+      breed_ids: breedId,
+    },
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import { fetchBreeds , fetchCatByBreed} from "./cat-api";
 
 const refs = {
     select: document.querySelector('.breed-select'),
@@ -6,33 +6,37 @@ const refs = {
     error: document.querySelector('.error'),
     catInfo: document.querySelector('.cat-info'),
   };
+  function markupData(arr) {
+    return arr.map( ({name, id} ) => `<option value="${id}">${name}</option>` ).join('') 
+    }
 
-const URL = 'https://api.thecatapi.com/v1';
-const BREEDS_ENDPOINT = '/breeds';
-const KEY = 'live_B9grJgv4HqzW6TZpqeLqnlyqi2vhD7LVDuhx5sgYT8L76L0WiuGpLioEPRoZa1XP';
-const INFO_ABOUT_CAT = '/images/search?breed_ids'
-
-function fetchBreeds(){
-    axios.get(URL + BREEDS_ENDPOINT, {
-        headers: {
-            'x-api-key': KEY,
-        }
-    }).then(resp=> 
-       refs.select.innerHTML = markupData(resp.data ) )
-    .catch(err => console.log(err))
+function fetch() {
+  fetchBreeds().
+    then(resp=> 
+        refs.select.innerHTML = markupData(resp.data ) ,
+        )
+     .catch(err => console.log(err))
 }
+fetch()
 
-fetchBreeds();
-// console.dir(refs.select);
-
-function markupData(arr) {
-return arr.map( ({name, id}) => `<option value="${id}">${name}</option>`).join('') 
-
+refs.select.addEventListener('change', () => {
+    const selectedOption = refs.select.options[refs.select.selectedIndex];
+    const selectedValue = selectedOption.value;    
+    fetchCatByBreed(selectedValue)
+    .then(resp=> 
+        refs.catInfo.innerHTML = descriptionCat(resp.data),)
+     .catch(err => console.log(err))
 }
+    )
 
-function fetchCatByBreed(breedId){
-axios(URL + INFO_ABOUT_CAT + breedId),{
-    headers: {
-        'x-api-key': KEY,
-    }}
-}
+    function descriptionCat(arr) {
+        return arr.map(({ url, breeds }) => {
+            const { name, description, temperament } = breeds[0];
+            return `
+                <img src="${url}" alt="${name}" width="900">
+                <h2>${name}</h2>
+                <p>${description}</p>
+                <p>Temperament: ${temperament}</p>
+            `;
+        }).join('');
+    }
